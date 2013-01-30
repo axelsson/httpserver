24f54c62cb90cec81c0100a309676edbd83a018d
johanna <johannaaxelsson90@gmail.com>
2013-01-30 13:06:49 +0100

    final version

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -1,27 +1,21 @@
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 import java.util.StringTokenizer;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-/* klient skickar get från webbläsare till server
- *  servern skapar en cookie för klienten och returnerar html
- *  klienten gissar svaret
- *  servern kopplar klient - cookie och svarar baserat på tidigare svar
- *  klienten svarar rätt, cookie tas bort
- *  servern säger grattis
- * */
 public class HttpServer{
 
-//	HttpCookie cookie = new HttpCookie("ett","tva");
-//	ConcurrentLinkedQueue<Game> games = new ConcurrentLinkedQueue<Game>();
-	ArrayList<Game> games = new ArrayList<Game>();
+	HashMap<Integer, Game> games = new HashMap();
+//	ArrayList<Game> games = new ArrayList<Game>();
 	int idCounter = 0;
 	boolean gotCookie = false;
 	String cookieHeader="";
 	int guess = -1;
-	
+	boolean victory = false;
+
 	public static void main(String[] args) throws IOException{
 		new HttpServer().start();
 	}
@@ -37,15 +31,20 @@ public class HttpServer{
 			String str = request.readLine();
 			System.out.println(str);
 			if (str.contains("guess")){
-				int index = str.indexOf("=");
-				int guess = Integer.parseInt(str.substring(index+1, index+2));
+				int begin = str.indexOf("=");
+				guess = Integer.parseInt(str.substring(begin+1, begin+3).trim());
 				System.out.println("guess: "+guess);
 			}
-//			StringTokenizer tokens = new StringTokenizer(str," ?");
-//			tokens.nextToken(); // Ordet GET
-//			String requestedDocument = tokens.nextToken();
-//			System.out.println("requested doc: " +requestedDocument);
-			
+			if (str.contains(".ico")){
+				s.close();
+				continue;
+			}
+
+			//			StringTokenizer tokens = new StringTokenizer(str," ?");
+			//			tokens.nextToken(); // Ordet GET
+			//			String requestedDocument = tokens.nextToken();
+			//			System.out.println("requested doc: " +requestedDocument);
+
 			while((str = request.readLine()) != null && str.length() > 0){
 				if (str.contains("Cookie")){
 					gotCookie = true;
@@ -56,19 +55,19 @@ public class HttpServer{
 			System.out.println("Förfrågan klar.");
 			s.shutdownInput();	//ger ett men kraschar efter en stund om man kör från egen dator
 			if (gotCookie == false){
-				idCounter++;
 				createCookie(s);		
 			}
 			else{
-				respond(guess, cookieHeader,s);
-				}
-			
+				respond(cookieHeader,s);
+			}
+
 
 		}
 	}
 	//Responds and creates a cookie for a new user. 
 	public void createCookie(Socket s) throws IOException{
 		System.out.println("Creating new cookie...");
+		idCounter++;
 		PrintStream response = new PrintStream(s.getOutputStream());
 		response.println("HTTP/1.1 200 OK");
 		response.println("Server : Johannas server");
@@ -76,9 +75,9 @@ public class HttpServer{
 		response.println("Set-Cookie: clientId="+idCounter+"; expires=Sunday,10-Feb-13 21:00:00 GMT");
 		//create new object game with idCouter.
 		Game newGame = new Game(idCounter);
-		games.add(newGame);
-		
-		File f = new File("C:/Users/Johanna/workspace/Iproglabb2/src/intnet.txt");	//Filenotfound
+		games.put(idCounter, newGame);
+
+		File f = new File("C:/Users/Johanna/workspace/Iproglabb2/src/intnet3.txt");	//Filenotfound
 		FileInputStream infil = new FileInputStream(f);
 		byte[] b = new byte[1024];
 		while( infil.available() > 0){
@@ -89,10 +88,10 @@ public class HttpServer{
 		s.close();
 		System.out.println("Cookie created and sent.");
 	}
-	
+
 	//Responds to requests sent from users with active games.
-	public void respond(int guess,String cookie, Socket s) throws IOException{
-		
+	public void respond(String cookie, Socket s) throws IOException{
+
 		//getting the sessionID of the player
 		int answer;
 		Game game = null;
@@ -100,66 +99,99 @@ public class HttpServer{
 		int index = cookie.indexOf("=");
 		int sessionID = Integer.parseInt(cookie.substring(index+1));
 		System.out.println("sessionID is: "+ sessionID);
-
-		for (Game current : games) {
-			if(current.getID()==sessionID){
-				answer = current.getAnswer();
-				game  = current;
-				state = getResult(answer, guess, current);
-			}
+		
+		if (games.containsKey(sessionID)){
+			game = games.get(sessionID);
+		}
+//		for (Game current : games) {
+//			if(current.getID()==sessionID){
+//				game  = current;
+//			}
+//		}
+		if (game == null){
+			createCookie(s);
+			return;
+		}
+		else{
+			answer = game.getAnswer();
+			System.out.println("här är svaret: "+answer);
+			state = getResult(answer, game);
 		}
 		//använd sessionID för att hämta game, returnera htmlsida med olika svar beroende på gissning
-		
+
 		System.out.println("Responding to client...");
 		PrintStream response = new PrintStream(s.getOutputStream());
 		response.println("HTTP/1.1 200 OK");
 		response.println("Server : Johannas server");
 		response.println("Content-Type: text/html");
 
-		
-		//om gissning rätt -> stäng connection och ta bort game
 		File f = new File("C:/Users/Johanna/workspace/Iproglabb2/src/intnet.txt");
 		FileInputStream infil = new FileInputStream(f);
 		byte[] b = new byte[1024];
 		while( infil.available() > 0){
 			response.write(b,0,infil.read(b));
 		}
-		response.println(state+"\n You have made "+game.turn + " number of guess(es).");
 		infil.close();
+		response.println(state+"<br> You have made "+game.getTurn() + " guess(es).");
+		File f2 = new File("C:/Users/Johanna/workspace/Iproglabb2/src/intnet2.txt");
+		FileInputStream infil2 = new FileInputStream(f2);
+		byte[] b2 = new byte[1024];
+		while( infil2.available() > 0){
+			response.write(b2,0,infil2.read(b2));
+		}
+		if (victory == true){
+			game.resetGame();
+			victory = false;
+			//generate new answer
+			//reset turn
+//			games.remove(sessionID);
+		}
+
+		infil2.close();
 		s.shutdownOutput();
 		s.close();
 	}
-	
-	public String getResult(int answer, int guess, Game game){
+
+	public String getResult(int answer, Game game){
 		String result = "Congratulations, you won!";		//win
-		if (answer > guess){//guess lower	
-			result = "Guess lower";
-			game.turn++;
+		System.out.println("här är gissningen: " + guess);
+		if (answer < guess){//guess higher	
+			result = "Guess lower.";
+			System.out.println("turn ändras1: "+game.getTurn());
+			game.increaseTurn();
 		}
-		else if(answer < guess){//guess higher
-			result = "Guess higher";
-			game.turn++;
+		else if(answer > guess){//guess lower
+			result = "Guess higher.";
+			System.out.println("turn ändras2: "+game.getTurn());
+			game.increaseTurn();
 		}
 		else{
-			games.remove(game);
+			victory = true;
+			game.increaseTurn();
 		}
 		return result;
 	}
 
-	//class Game represents an ongoing game, storing sessionID and guess.
-//	skapa: skapa answer och sessionID
-//	spela:  hitta rätt game mha sessionID
-//			hämta answer
-//			om fel, uppdatera gissningar
+
 	private class Game{
 		int sessionID;
 		int answer;
 		int turn = 0;
 		Random r = new Random();
-		String message;
+		String message = "";
 		public Game(int ID){
 			sessionID = ID;
 			answer = r.nextInt(101);
+		}
+		public void resetGame(){
+			answer = r.nextInt(101);
+			turn = 0;
+		}
+		public void increaseTurn(){
+			turn++;
+		}
+		public int getTurn(){
+			return turn;
 		}
 		public int getID(){
 			return sessionID;
@@ -167,7 +199,5 @@ public class HttpServer{
 		public int getAnswer(){
 			return answer;
 		}
-
-
 	}
 }
